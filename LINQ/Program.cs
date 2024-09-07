dd4a4c65e05e9ec34a5ee6bcb41da6874a24adda
Chris Gaudet <chrisgaudet9@gmail.com>
2024-09-06 19:12:24 -0500

    completed linq exer 2

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace LINQ
+﻿using System.Linq;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var videoGames = new List<string>() { "Super Mario", "Crash Bandicoot", "Horizon"};
+
+            var games = videoGames.OrderBy(name => name.Length);
+
+            foreach (var game in games)
+            {
+                Console.WriteLine(game);
+            }
         }
     }
 }
